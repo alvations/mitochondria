765010cffba32e49f8ca94afcff4bec4734066cf
alvations <alvations@gmail.com>
2018-06-04 13:25:13 +0800

    up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'mitochondria',
   packages = ['mitochondria'],
-  version = '0.0.6',
+  version = '0.0.7',
   description = '',
   author = 'Liling Tan',
   license = 'MIT',
