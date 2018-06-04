2059ca10f527b0a790e8f03dfe8458cffa01d382
alvations <alvations@gmail.com>
2018-06-04 11:38:18 +0800

    upgrade the version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'mitochondria',
   packages = ['mitochondria'],
-  version = '0.0.4',
+  version = '0.0.5',
   description = '',
   author = 'Liling Tan',
   license = 'MIT',
